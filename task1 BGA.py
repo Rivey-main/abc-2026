68dcddc362564128840d3d73fbb73ba974813f83
Rivey-main <baturin.grixa@gmail.com>
2026-09-16 18:43:38 +0300

    Add files via upload

---
+++ task1 BGA.py
@@ -0,0 +1,31 @@
+# todo: Определить в коде переменные:
+# 1. Целочисленного типа
+# 2. Вещественного типа
+# 3. Логического типа
+# 4. Строкового типа
+# 5. Пустого типа
+# Вывести их типы.
+
+
+# 1. Целочисленный тип (integer)
+integer_var = 42
+
+# 2. Вещественный тип (float)
+float_var = 3.14
+
+# 3. Логический тип (boolean)
+boolean_var = True
+
+# 4. Строковый тип (string)
+string_var = "Привет, мир!"
+
+# 5. Пустой тип (NoneType)
+none_var = None
+
+# Вывод типов переменных
+print("1.", type(integer_var))
+print("2.", type(float_var))
+print("3.", type(boolean_var))
+print("4.", type(string_var))
+print("5.", type(none_var))
+
